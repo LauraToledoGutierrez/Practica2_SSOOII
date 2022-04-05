106907827ef501c439760245e763c04c0f469e7a
KillerDMS <DanielMax.Samson@alu.uclm.es>
2022-04-05 13:02:47 +0200

    falta buscarPalabra

--- ssooiigle.cpp
+++ ssooiigle.cpp
@@ -11,11 +11,12 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <queue>
+#include <functional>
 
 std::fstream fp;
-void leerFichero(std::string rutaFichero);
+std::vector<std::string> leerFichero(std::string rutaFichero);
 int leerLineas();
-
+void buscarPalabra(std::vector<std::string> vector, Buscador buscador);
 int main(int argc, char *argv[]){
     
     if(argc!=4){
@@ -28,27 +29,40 @@ int main(int argc, char *argv[]){
 
     std::vector<std::thread> vhilos;
     std::string rutaFichero(argv[1]); 
-    leerFichero(rutaFichero);
-    int numeroLineas= leerLineas();
+    std::vector<std::string> lineas= leerFichero(rutaFichero);
+    int numeroLineas= lineas.size();
 
 
     for (unsigned i=0; i<numeroHilos-1;i++){
         int lim1=((numeroHilos/numeroHilos)*i);
-        int lim2=((numeroLineas/numeroHilos)*(i+1))+1;
-        vhilos.push_back(std::thread(buscarPalabra,lim1,lim2)); //Todo funcion que ejecute cada hilo
+        int lim2=(lim1+(numeroLineas/numeroHilos)-1);
+        std::vector<std::string> vectorParcial;
+
+        Buscador buscador(palabraBuscada, i, lim1, lim2);
+
+        for(int j=lim1; j<lim2;j++){
+            vectorParcial.push_back(lineas[j]);
+        }
+
+        vhilos.push_back(std::thread(buscarPalabra, vectorParcial, buscador)); //Todo funcion que ejecute cada hilo
     }
+    std::for_each(vhilos.begin(), vhilos.end(), std::mem_fn(&std::thread::join));
     
 }
 
 
-void leerFichero(std::string rutaFichero){
-    fp.open(rutaFichero);
-    if (!fp.is_open()) {
-        std::cout <<"Error al abrir " << rutaFichero << "\n";
-        exit(EXIT_FAILURE);
+std::vector<std::string> leerFichero(std::string rutaFichero){
+    std::ifstream archivo_entrada(rutaFichero);
+    std::string linea;
+    std::vector<std::string> lineas;
+
+    while(getline(archivo_entrada, linea)){
+        lineas.push_back(linea);
     }
+    return lineas;
 }
 
+/* DEPRECATED
 int leerLineas(){
     int lineasFichero=0;
     char cadena[1024];
@@ -63,22 +77,42 @@ int leerLineas(){
     fp.close();
     return lineasFichero;
 }
+*/
 
-void buscarPalabra(int lim1, int lim2){
-    
+void buscarPalabra(std::vector<std::string> vector, Buscador buscador){
+    //Meter en vector las lineas enteras de todo el fichero (global)
+    //buscarPalabra solo busque entre lim1 y lim2 (posiciones del vector)
+    //Busqueda de cada linea va a ser local para buscar las palabras (solo va a salir la palabra anterio y posterior y ya)
+    //Comprobar si la palabra es la ultima de una linea, que busque en la linea siguiente
+    std::vector<std::string> palabras;
+
+    for(int i=0; i<vector.size(); i++){
+
+    }
+
 }
 
 class Buscador
 {
+    public:
+    Buscador(std::string, int, int, int);
     std::string palabraBuscada;
     int id;
     int lineaInicio;
     int lineaFinal;
     std::queue<ResultadoBusqueda> colaResultados;
 };
+Buscador::Buscador(std::string palabraBuscada, int id, int lineaInicio, int lineaFinal)
+{
+    this->palabraBuscada= palabraBuscada;
+    this->id= id;
+    this->lineaInicio=lineaInicio;
+    this->lineaFinal=lineaFinal;
+}
 
 class ResultadoBusqueda{
     std::string palabraAnterior;
     std::string palabraPosterior;
     int lineaResultado;
 };
+
