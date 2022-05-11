170be59545c16145a704f473b7df66ce27e06306
KillerDMS <DanielMax.Samson@alu.uclm.es>
2022-05-11 13:52:26 +0200

    Avances Dani finder.h

--- include/finder.h
+++ include/finder.h
@@ -10,8 +10,31 @@ class Finder
         int initialLine;
         int finalLine;
         std::queue<Search_Result> queueResults;
-
+        std::vector<std::string> books = {"17-LEYES-DEL-TRABJO-EN-EQUIPO.txt", "21-LEYES-DEL-LIDERAZGO.txt", "25-MANERAS-DE-GANARSE-A-LA-GENTE.txt",
+                        "ACTITUD-DE-VENDEDOR.txt", "El-oro-y-la-ceniza.txt", "La-última-sirena.txt", "prueba.txt", 
+                        "SEAMOS-PERSONAS-DE-INFLUENCIA.txt", "VIVE-TU-SUEÑO.txt"};
+        std::atomic<int> id_request()
     public:
+
+    void operator() () const{
+        //!NEcesitamos exclusion mutua para sacar de la cola de peticiones
+        cvClient.wait(uniLockQRequests, [] {return !clientRequestFree.empty() || !clientRequestPremium.empty();});
+        int random= 1+ rand()%(100-1);
+        Request req;
+        if(random<=79 & !clientRequestPremium.empty()){
+            req=std::move(clientRequestPremium.front());
+            clientRequestPremium.pop();
+        }else if(random>79 & !clientRequestFree.empty()){
+            req=std::move(clientRequestFree.front());
+            clientRequestFree.pop();
+        }else{
+            std::cout<<"No hay peticiones disponibles"<<std::endl;
+        }
+
+
+
+
+    }
         Finder(){}
         Finder(std::string palabra, int Id, int firstLine, int lastLine): searchedWord(palabra), id(Id), initialLine(firstLine), finalLine(lastLine){}
         
