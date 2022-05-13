d0a9b9889690c59936068562895335f71c0a1e4b
KillerDMS <DanielMax.Samson@alu.uclm.es>
2022-05-13 16:57:54 +0200

    comentarios finder.h

--- include/finder.h
+++ include/finder.h
@@ -12,129 +12,111 @@ extern std::vector<std::string> readFile(std::string bookPath);
 extern void findWord(int nbook, int iteration, std::vector<std::string> vector, int lowerLimit, int uppwerLimit, Request requestCurrent);
 extern void systemPay();
 
-//Request req(0,0,"",0);
-
 class Finder
 {
+    /***********************************
+     * Class: Finder
+     * Description: This class is the finder that searches the word in the method findWord in the main class. Creates the "Master" Finder threads
+     * which funcionality is to find the word in all of books 
+     * Date: 13/05/2022
+     ***********************************/
 private:
-    std::string searchedWord;
-    int id;
-    int initialLine;
-    int finalLine;
-
+    std::string searchedWord;       //Word that the client want to search in all books
+    int id; 
+    int initialLine;                // First line of the book
+    int finalLine;                  //The last line of the book
 
 public:
     std::vector<std::string> books = {"./Libros_P2/17-LEYES-DEL-TRABJO-EN-EQUIPO.txt", "./Libros_P2/21-LEYES-DEL-LIDERAZGO.txt", "./Libros_P2/25-MANERAS-DE-GANARSE-A-LA-GENTE.txt",
 "./Libros_P2/ACTITUD-DE-VENDEDOR.txt", "./Libros_P2/El-oro-y-la-ceniza.txt", "./Libros_P2/La-última-sirena.txt", "./Libros_P2/prueba.txt",
-"./Libros_P2/SEAMOS-PERSONAS-DE-INFLUENCIA.txt", "./Libros_P2/VIVE-TU-SUEÑO.txt"};
-    int tamanio= books.size();
-    std::vector<std::thread> vFinderThreads;
+"./Libros_P2/SEAMOS-PERSONAS-DE-INFLUENCIA.txt", "./Libros_P2/VIVE-TU-SUEÑO.txt"};  //Vector of all the available books/sources it can be searched into
+    std::vector<std::thread> vFinderThreads; //Vector of Finder threads
 
     /***********************************
-     * Method: createFinderThreads
-     * Description: Divides a source into several parts so that each thread can look into a part of the source file
-     * Parameters: numberLines(number of total lines of the source file), wordToSearch(word that we are searching in the files), partialLines(all the lines a thread must look into)
+     * Method: operator()
+     * Description: The finder takes the Request from the queue clientRequestFree and clientRequestPremium and creates the "children" Finder threads
+     * one for each book which execute the method findWord
+     * 
      * Version: 1.0
-     * Date: 10/04/2022
-
-    void Finder::createFinderThread(int numberLines, std::string wordToSearch, std::vector<std::string> partialLines)
-    {
-
-        std::for_each(vFinderThreads.begin(), vFinderThreads.end(), std::mem_fn(&std::thread::join));
-    }
+     * Date: 13/05/2022
      ***********************************/
     void operator()() 
     {
-        Request req(0,0,"",0);
-        std::mutex mutexNOSE;
-        Search_Result sr;
-       
-        //! NEcesitamos exclusion mutua para sacar de la cola de peticiones
+        Request req(0,0,"",0); //An object of Request
+        std::mutex mutex; //Mutex
+        Search_Result sr; //An object of Search_Results
 
+    
         while (1)
         {
             cvClient.wait(uniLockQRequests, []
-                          { return !clientRequestFree.empty() || !clientRequestPremium.empty(); });
-            int random = 1 + rand() % (100 - 1);
-           // Request req;
-            std::unique_lock<std::mutex> lck (mutexNOSE);
+                        { return !clientRequestFree.empty() || !clientRequestPremium.empty(); });
+            std::unique_lock<std::mutex> lck (mutex);
 
-            if (random <= 79 && !clientRequestPremium.empty())
+            if (!clientRequestPremium.empty())
             {
-                //std::cout<<"ME ABURRO"<<std::endl;
                 req = clientRequestPremium.front(); 
-                //std::cout << "nose "<<req.getIdClient()<<std::endl;
-
-                //std::unique_lock<std::mutex> lck (mutexNOSE);
                 clientRequestPremium.pop_back();
             }
             
-            else if (random > 79 && !clientRequestFree.empty())
+            else if (!clientRequestFree.empty())
             {
-                //std::cout<<"ME ABURRO x2"<<std::endl;
                 req = clientRequestFree.front();
-                //std::cout << "nose "<<req.getIdClient()<<std::endl;
-                //std::unique_lock<std::mutex> lck (mutexNOSE);
                 clientRequestFree.pop_back();
             }
             else
             {
-                std::cout << "No available requests" << std::endl;
+                std::cout << "No available requests" << std::endl; //The queues are empty
             }
             // Functionality Children Finder
             
             for (int i = 0; i < books.size(); i++)
             {
                 std::vector<std::string> fileToRead = readFile(books[i]);
-                
-                //std::cout<<"LINREAS"<<lines<<std::endl;
-                //std::cout << "aaaaaaaaaaaaaaaaaaaaaaaa" <<std::endl;
 
                 // METHOD CREATEFINDERTHREADS
                 for (int j = 0; j < NUMBERTHREADS; j++)
                 {
-                    int lowerLimit, upperLimit;
+                    int lowerLimit, upperLimit; //Limits 
 
-                    // Si es el primer hilo
+                    // If is the first thread
                     if (j == 0)
                         lowerLimit = 1;
                     else
                         lowerLimit = ((fileToRead.size() / NUMBERTHREADS) * j + 1);
 
-                    // Si es el ultimo hilo
+                    // If is the last thread
                     if (j == NUMBERTHREADS - 1)
                         upperLimit = fileToRead.size();
                     else
                         upperLimit = (lowerLimit + (fileToRead.size() / NUMBERTHREADS) - 1);
 
-                    // Creamos el objeto finder con sus correspondientes valores
-                    //std::cout <<"ALGO "<<upperLimit<<std::endl;
+                    // Create an object Finder with her values
                     Finder finder(req.getwordToSearch(), j, lowerLimit, upperLimit);
-                    //threadFinder.push_back(finder);
-
-                    // Cada hilo realizara el metodo de buscarPalabra
-                    //std::cout << "aaaaaaaaaaaaaaaaaaaaaaaa" <<std::endl;
+                    // All of thread execute the method findWord 
                     vFinderThreads.push_back(std::thread(findWord, i, j, fileToRead, lowerLimit, upperLimit, req));
-                    while (1)
-                    {
-                        cvFinder.wait(uniLockFinderChildren, [] {return !requestsDone.empty() ;});
-                        std::cout<<"oooooooooooooooooo"<<std::endl;
-                        if (req.getEndRequest())
-                        {
-                            requestsDone.push_back(req);
-                            std::for_each(vFinderThreads.begin(), vFinderThreads.end(), std::mem_fn(&std::thread::join));
-                            break;
-                        }
-                        else if (!req.getEndRequest())
-                        {
-                            systemPay();
-                        }
-                    }
                 }
-                std::for_each(vFinderThreads.begin(), vFinderThreads.end(), std::mem_fn(&std::thread::detach));
+            }
+            while (1)
+            {
+                cvFinder.wait(uniLockFinderChildren, [] {return !requestsDone.empty() ;});
+                        
+                if (req.getEndRequest()) //The request is finished
+                {
+                    requestsDone.push_back(req); 
+                    break;
+                }
+                else if (!req.getEndRequest())  //The request isn't finished
+                {
+                    systemPay();
+                }
             }
         }
+        std::for_each(vFinderThreads.begin(), vFinderThreads.end(), std::mem_fn(&std::thread::join));
     }
+
+    // Constructor, getters and setters
+
     Finder() {}
     Finder(std::string palabra, int Id, int firstLine, int lastLine) : searchedWord(palabra), id(Id), initialLine(firstLine), finalLine(lastLine) {}
 
